9b97205d9e0287095ab419826dbf6838d5268934
BrianTheWiz <brian.howard@greenparty.ca>
2020-02-13 15:17:59 -0500

    typo

--- tests/Message/PurchaseResponseTest.php
+++ tests/Message/PurchaseResponseTest.php
@@ -30,7 +30,7 @@ class ResponseTest extends TestCase
         $this->assertFalse($response->isSuccessful());
         $this->assertFalse($response->isRedirect());
         $this->assertEmpty($response->getTransactionId());
-        $this->assertEmpty($response-getCustomerCode());
+        $this->assertEmpty($response->getCustomerCode());
         $this->assertSame('Credit card is invalid', $response->getMessage());
     }
 }
