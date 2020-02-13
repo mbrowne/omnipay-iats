376a0f7ec9ee6bfdd6765c1711ceb135c8c6b8e9
BrianTheWiz <brian.howard@greenparty.ca>
2020-02-13 14:29:48 -0500

    .

--- src/Message/PurchaseResponse.php
+++ src/Message/PurchaseResponse.php
@@ -17,7 +17,7 @@ class PurchaseResponse extends AbstractResponse
     
     public function isSuccessful()
     {
-        var_dump($this->data);
+        print_r($this->data);
         return (strpos(trim($this->data['AUTHORIZATIONRESULT']), 'OK') === 0);
     }
     
